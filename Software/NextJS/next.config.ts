1e930741c953cd01c1c89392ab820102972a115f
Isaac Spool <spool.i@northeastern.edu>
2025-03-20 18:17:56 -0400

    Changed next config to work with rclnodejs

--- Software/NextJS/next.config.ts
+++ Software/NextJS/next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
+import type { Configuration } from "webpack";
 
 const nextConfig: NextConfig = {
     output: "standalone",
+    serverExternalPackages: ["rclnodejs"],
+    webpack(config: Configuration, { isServer }: { isServer: boolean }) {
+        // Ignore source map files to prevent parsing issues
+        config.module?.rules?.push({
+            test: /\.js\.map$/,
+            loader: "ignore-loader",
+        });
+        if (isServer) {
+            // Ensure the config.externals is typed properly
+            config.externals = [
+                {
+                    rclnodejs: "commonjs rclnodejs", // Treat rclnodejs as a commonjs external
+                },
+            ];
+        }
+
+        return config;
+    },
 };
 
 export default nextConfig;
